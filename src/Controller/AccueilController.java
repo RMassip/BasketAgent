cc414bde563be9bb50298f0917d9947105db4b94
RMassip <romain31.massip@orange.fr>
2023-11-09 12:20:47 +0100

    V0.1

---
+++ src/Controller/AccueilController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AccueilController {
+
+    @FXML
+    private Button btnContinue;
+
+    @FXML
+    private Button btnNewP;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    void btnContinueOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewPOnClick(ActionEvent event) {
+
+    }
+
+}
+
